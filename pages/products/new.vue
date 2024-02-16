8e8d880dc876d14cacdb69e51b19373bfd67f0b5
Arkadiusz Zielinski <arkadiusz.zielinski@respect.energy>
2024-02-16 12:41:08 +0100

    new

---
+++ pages/products/new.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+ Damn
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/products/single.vue
+++ pages/products/single.vue
@@ -2,7 +2,7 @@
     <div>
 
 
-        <h1>hi</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quod? Necessitatibus quisquam inventore dicta mollitia nesciunt vero rem, ratione delectus? Necessitatibus illo tempore quos inventore, doloribus neque animi laudantium impedit.</h1>
 
     </div>
 </template>
