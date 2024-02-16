684d3d43c9de43426516a06437e0e1500073ac4f
Arkadiusz Zielinski <arkadiusz.zielinski@respect.energy>
2024-02-16 15:17:53 +0100

    update2

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,16 @@ const route = useRoute()
 <mode />
 
   <div>
+    <div>
+
+      
+
+    </div>
     <h1>Nuxt Routing set up successfully!</h1>
-    <h2 class="font-extrabold">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam magni unde voluptatem nostrum id molestiae, ab exercitationem non ullam dolores neque accusamus pariatur ducimus assumenda harum ex, reiciendis amet architecto.</h2>
+    <h2 class="font-extrabold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis maiores voluptatem nisi vero et. Hic ea consectetur voluptate, perspiciatis dolores rerum assumenda blanditiis beatae quam aliquid voluptatibus sequi commodi suscipit? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam magni unde voluptatem nostrum id molestiae, ab exercitationem non ullam dolores neque accusamus pariatur ducimus assumenda harum ex, reiciendis amet architecto.</h2>
     <p>Current route: {{ route.path }}</p>
     <a href="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</a>
   </div>
 </template>
+
 
