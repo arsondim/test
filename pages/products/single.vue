896c62b0764d2aacd833173f1e80b30249f01844
Arkadiusz Zielinski <arkadiusz.zielinski@respect.energy>
2024-02-16 12:33:51 +0100

    first commit

---
+++ pages/products/single.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+
+        <h1>hi</h1>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
